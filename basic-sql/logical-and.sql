1679c81e092929c8eddfad083279063a6ec4c51d
Harvy Jones Pontillas <harvyjonespontillas@gmail.com>
2025-05-20 20:52:35 +0800

    logical and

--- basic-sql/logical-and.sql
+++ basic-sql/logical-and.sql
@@ -3,3 +3,15 @@ SELECT *
   FROM tutorial.billboard_top_100_year_end
 WHERE year_rank <= 10
   AND group_name ILIKE '%ludacris%'
+
+-- Query that surfaces the top-ranked records in 1990, 2000, and 2010.
+SELECT *
+  FROM tutorial.billboard_top_100_year_end
+WHERE year_rank = 1
+  AND year IN (1990, 2000, 2010)
+
+-- Query that lists all songs from the 1960s with "love" in the title.
+SELECT *
+  FROM tutorial.billboard_top_100_year_end
+WHERE year BETWEEN 1960 AND 1969
+  AND song_name ILIKE '%love%'
