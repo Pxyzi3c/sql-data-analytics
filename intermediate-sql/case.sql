676caba5800398bb3f70d17a41c4923d0b613ee6
Harvy Jones Pontillas <harvyjonespontillas@gmail.com>
2025-05-27 22:16:09 +0800

    final case practice

--- intermediate-sql/case.sql
+++ intermediate-sql/case.sql
@@ -47,3 +47,23 @@ FROM benn.college_football_players
 WHERE state = 'CA'
 GROUP BY state, year
 ORDER BY players_category
+
+-- Query that displays the number of players in each state, with FR, SO, JR, and SR players in separate columns and another column for the total number of players. Order results such that states with the most players come first.
+SELECT state, 
+  COUNT(CASE WHEN year = 'FR' THEN 1 ELSE NULL END) AS fr_count,
+  COUNT(CASE WHEN year = 'SO' THEN 1 ELSE NULL END) AS so_count,
+  COUNT(CASE WHEN year = 'JR' THEN 1 ELSE NULL END) AS jr_count,
+  COUNT(CASE WHEN year = 'SR' THEN 1 ELSE NULL END) AS sr_count,
+  COUNT(*) AS number_of_players
+FROM benn.college_football_players
+GROUP BY state
+ORDER BY number_of_players DESC
+
+-- Query that shows the number of players at schools with names that start with A through M, and the number at schools with names starting with N - Z.
+SELECT school_name, 
+  CASE WHEN school_name < 'n' THEN 'A-M'
+    WHEN school_name >= 'n' THEN 'N-Z'
+    ELSE NULL END AS school_category, 
+  COUNT(1) AS players
+FROM benn.college_football_players
+GROUP BY school_name
