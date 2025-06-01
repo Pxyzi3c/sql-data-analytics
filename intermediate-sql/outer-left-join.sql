20b8b1ac0b27d50581a9acdcf25dfd2e953e5d7f
Harvy Jones Pontillas <harvyjonespontillas@gmail.com>
2025-06-02 01:04:44 +0800

    using multiple keys on join

--- intermediate-sql/outer-left-join.sql
+++ intermediate-sql/outer-left-join.sql
@@ -2,23 +2,21 @@
 SELECT
   COUNT(a.company_permalink) AS acquisition_count,
   COUNT(c.permalink) AS company_count
-FROM
-  tutorial.crunchbase_acquisitions AS a
-  LEFT JOIN tutorial.crunchbase_companies AS c ON a.company_permalink = c.permalink
-  --  Count the number of unique companies (don't double-count companies) and unique acquired companies by state. Do not include results for which there is no state data, and order by the number of acquired companies from highest to lowest. 
+FROM tutorial.crunchbase_acquisitions AS a
+LEFT JOIN tutorial.crunchbase_companies AS c 
+ON a.company_permalink = c.permalink
+
+--  Count the number of unique companies (don't double-count companies) and unique acquired companies by state. Do not include results for which there is no state data, and order by the number of acquired companies from highest to lowest. 
 SELECT
   c.state_code,
   COUNT(DISTINCT c.name) AS company_count,
   COUNT(DISTINCT a.company_name) AS acquired_companies_count
-FROM
-  tutorial.crunchbase_companies AS c
-  LEFT JOIN tutorial.crunchbase_acquisitions AS a ON c.permalink = a.company_permalink
-WHERE
-  c.state_code IS NOT NULL
-GROUP BY
-  c.state_code
-ORDER BY
-  acquired_companies_count DESC
+FROM tutorial.crunchbase_companies AS c
+LEFT JOIN tutorial.crunchbase_acquisitions AS a 
+ON c.permalink = a.company_permalink
+WHERE c.state_code IS NOT NULL
+GROUP BY c.state_code
+ORDER BY acquired_companies_count DESC
 
 -- Query that shows the name, status, and category code of companies that were founded more than 5 years before they were acquired.
 SELECT
@@ -26,12 +24,19 @@ SELECT
   c.status AS status,
   c.category_code as category_code,
   COUNT(i.investor_permalink) AS investors
-FROM
-  tutorial.crunchbase_companies AS c
-  LEFT JOIN tutorial.crunchbase_investments AS i ON c.permalink = i.company_permalink
-WHERE
-  c.founded_year + 5 < i.funded_year
+FROM tutorial.crunchbase_companies AS c
+LEFT JOIN tutorial.crunchbase_investments AS i 
+ON c.permalink = i.company_permalink
+WHERE c.founded_year + 5 < i.funded_year
 GROUP BY
   c.name,
   c.status,
   c.category_code
+
+-- Query that shows the name and permalink of companies that were acquired by investors. Using multiple keys in the ON clause.
+SELECT c.name AS company_name,
+  i.company_permalink AS company_permalink
+FROM tutorial.crunchbase_companies AS c
+LEFT JOIN tutorial.crunchbase_investments AS i
+ON c.permalink = i.company_permalink
+AND c.name = i.company_name
