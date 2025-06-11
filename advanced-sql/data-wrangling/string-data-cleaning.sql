af18cf4273bf6a719a39b223064fc84341d1822c
Harvy Jones Pontillas <harvyjonespontillas@gmail.com>
2025-06-12 03:58:50 +0800

    practice data cleaning

--- advanced-sql/data-wrangling/string-data-cleaning.sql
+++ advanced-sql/data-wrangling/string-data-cleaning.sql
@@ -1,4 +1,4 @@
--- PRACTICE:
+-- SUMMARY:
 SELECT
     incidnt_num,
     date,
@@ -14,3 +14,26 @@ SELECT
     POSITION('a' IN LOWER(descript)) AS a_position
 FROM
     tutorial.sf_crime_incidents_2014_01
+
+-- =============================================================================
+-- EXERCISES
+-- =============================================================================
+
+-- Query that separates the `location` field into separate fields for latitude and longitude.
+-- You can compare your results against the actual `lat` and `lon` fields in the table.
+WITH CleanedLocation AS (
+    SELECT
+        location,
+        lat,
+        lon,
+        TRIM(BOTH '()' FROM location) AS cleaned_loc,
+        POSITION(',' IN TRIM(BOTH '()' FROM location)) AS comma_pos
+    FROM tutorial.sf_crime_incidents_2014_01
+)
+SELECT 
+    cl.location,
+    cl.lat,
+    cl.lon,
+    LEFT(cl.cleaned_loc, cl.comma_pos - 1) AS lat_str,
+    RIGHT(cl.cleaned_loc, LENGTH(cl.cleaned_loc) - cl.comma_pos - 1) AS lon_str
+FROM CleanedLocation AS cl
