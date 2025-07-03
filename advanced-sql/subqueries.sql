a688dcde34ddf8cecd173f0aa123d3369133fbad
Harvy Jones Pontillas <harvyjonespontillas@gmail.com>
2025-07-04 04:25:43 +0800

    full join two aggregated tables - sub queries

--- advanced-sql/subqueries.sql
+++ advanced-sql/subqueries.sql
@@ -43,7 +43,9 @@ FROM (
 ) sub
 WHERE sub.resolution ILIKE 'none'
 
--- Query that displays the average number of monthly incidents for each category. 
+-- =============================================================================
+-- Query that displays the average number of monthly incidents for each category.
+-- =============================================================================
 SELECT 
     sub.category,
     AVG(sub.incidents) AS avg_incidents
@@ -57,7 +59,9 @@ FROM (
 ) AS sub
 GROUP BY category
 
+-- =============================================================================
 -- Query that displays all rows from the three categories with the fewest incidents reported.
+-- =============================================================================
 SELECT 
   incidents.*,
   sub.incidents_count
@@ -71,3 +75,29 @@ INNER JOIN (
     LIMIT 3 
 ) AS sub
 ON incidents.category = sub.category
+
+-- =============================================================================
+-- Write a query that counts the number of companies founded and acquired by quarter starting in Q1 2012. 
+-- Create the aggregations in two separate queries, then join them.
+-- =============================================================================
+SELECT 
+    COALESCE(f.founded_quarter, a.acquired_quarter) AS quarter,
+    COALESCE(f.companies_founded, 0) AS companies_founded,
+    COALESCE(a.companies_acquired, 0) AS companies_acquired
+FROM (
+        SELECT 
+            founded_quarter,
+            COUNT(permalink) as companies_founded
+        FROM tutorial.crunchbase_companies
+        WHERE founded_quarter >= '2012-Q1'
+        GROUP BY founded_quarter
+    ) AS f
+FULL JOIN (
+        SELECT acquired_quarter,
+            COUNT(company_permalink) as companies_acquired
+        FROM tutorial.crunchbase_acquisitions
+        WHERE acquired_quarter >= '2012-Q1'
+        GROUP BY acquired_quarter
+    ) AS a
+ON f.founded_quarter = a.acquired_quarter
+ORDER BY "quarter"
