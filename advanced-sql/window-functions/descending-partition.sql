1ca0852417169ed39f0d7b8203a9b0f28458b7e1
Harvy Jones Pontillas <harvyjonespontillas@gmail.com>
2025-07-10 02:32:58 +0800

    add to do

--- advanced-sql/window-functions/descending-partition.sql
+++ advanced-sql/window-functions/descending-partition.sql
@@ -8,3 +8,8 @@ SELECT
     (PARTITION BY end_terminal ORDER BY duration_seconds DESC) AS total_duration
 FROM tutorial.dc_bikeshare_q1_2012
 WHERE start_time < '2012-01-08'
+
+-- =============================================================================
+-- TO-DO:
+-- https://mode.com/blog/most-popular-window-functions-and-how-to-use-them?utm_medium=referral&utm_source=mode-site&utm_campaign=sql-tutorial
+-- =============================================================================
